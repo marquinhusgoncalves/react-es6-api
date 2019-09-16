2a46d7c42f0bf81db4d0a3b9515d89332c72274d
Marcus <eu@marquinhusgoncalves.com>
2019-09-15 21:19:46 -0300

    chore: storing data in component state via ajax call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+
+  componentWillMount() {
+    axios('https://api.randomuser.me/?nat=US&results=5')
+    .then(response => this.setState({
+      users: response.data.results
+    }))
+  }
+
+  render() {
+    return <div className="App">React</div>
+  };
 }
 
 export default App;
